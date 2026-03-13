8c6e13ff6e8b6c1fb18d762c5ee6edcc34582e07
tiaL-ops <landytiaa@gmail.com>
2026-03-12 22:09:33 -0400

    Modified front

--- frontend/game.js
+++ frontend/game.js
@@ -1,8 +1,15 @@
 // Game configuration
 const config = {
   type: Phaser.AUTO,
+  parent: 'game-container',
   width: 960,
   height: 640,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 960,
+    height: 640
+  },
   physics: {
     default: 'arcade',
     arcade: {
